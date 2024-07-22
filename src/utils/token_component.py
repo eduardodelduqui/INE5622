ea23d3ec6fa2e1f164ae873e018f84e68da788d4
Carlos Aranha <carloseduardosiar@outlook.com>
2024-07-22 13:31:32 -0300

    ...

---
+++ src/utils/token_component.py
@@ -0,0 +1,21 @@
+class Token:
+  def __init__(self, type, value, line, column):
+    self.type = type
+    self.value = value
+    self.line = line
+    self.column = column
+
+  def __repr__(self):
+    if self.type == "id":
+      return "id"
+    elif self.type == "num":
+      return "num"
+    else:
+      return self.value
+  def toString(self):
+    if self.type == "id":
+      return "id"
+    elif self.type == "num":
+      return "num"
+    else:
+      return self.value
